dbaf97d72dd75ee39fc518ed84e78fb58947ca3e
Nguyễn Đình Minh Đăng <minhdang22082003@gmail.com>
2026-03-27 01:47:49 +0700

    chore: secure firebase keys, use env vars and update gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,14 +7,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArG3bEUNnrquDr0uMwUMGpMHgnfqwaL-0",
-  authDomain: "imguploadswp.firebaseapp.com",
-  projectId: "imguploadswp",
-  storageBucket: "imguploadswp.appspot.com",
-  messagingSenderId: "245122206178",
-  appId: "1:245122206178:web:c841d3a17e4bb8c2b2d1bb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const imageDb = getStorage(app); 
+export const imageDb = getStorage(app);
